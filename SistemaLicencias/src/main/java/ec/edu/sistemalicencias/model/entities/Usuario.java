a21c77a72720cd4ccb1a3f6098cd20c36342885a
Adonis_Lopez <adonis.lopez@epn.edu.ec>
2026-01-08 20:47:09 -0500

    Model usuario

---
+++ SistemaLicencias/src/main/java/ec/edu/sistemalicencias/model/entities/Usuario.java
@@ -0,0 +1,58 @@
+package ec.edu.sistemalicencias.model.entities;
+
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String passwordHash;
+    private String rol;
+    private boolean estado;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
